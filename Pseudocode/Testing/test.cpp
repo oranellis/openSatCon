b6a4d2b8cebf3e3e7142c9153738baa85cf2b02b
Aidan <2306781m@student.gla.ac.uk>
2022-02-12 10:32:32 +0000

    setting up control laws for pointing

--- Pseudocode/Testing/test.cpp
+++ Pseudocode/Testing/test.cpp
@@ -2,18 +2,21 @@
 #include <vector>
 #include <string>
 #include <math.h>
+#include <complex>
+//#include <QtMath>
+//#include <QGenericMatrix>
 
 
-
-#define PI=3.14159265
-int main(){
-
-  double x, y, result;
-  x = -10.0;
-  y = 10.0;
-  result = atan2 (y,x) * 180 / 3.14159265;
-  printf ("The arc tangent for (x=%f, y=%f) is %f degrees\n", x, y, result );
-  return 0;
-
-
+int rad2deg(double deg){
+    double rad;
+    rad=180/M_PI;
+    return rad;
 }
+
+int main ()
+{
+  double MomentOfInertia[3][3];
+  int ReactionWheelConfigMatrix[3][4]={{1,0,0,1},{0,1,0,1},{0,0,1,1}};
+  double omega_dot, TorqueCommand, TorqueDisturbances, TorqueActuators, ReactionWheelAngularMomentum;
+}
+
