1f305c1d0912e0ed835bea21955c8b2f64b54cf5
Shreyas Raje <2313623R@student.gla.ac.uk>
2022-04-12 14:40:03 +0100

    craft json format and moments of inertia calcs

---
+++ osc-daemon/Craft Build/craftcalcs.cpp
@@ -0,0 +1,51 @@
+#include <json/value.h>
+#include "fstream"
+#include <iostream>
+#include <math.h>
+
+std::ifstream craft_file("craft.json", std::ifstream::binary);
+craft_file >> craft;
+
+// access data via craft["ThrusterComponents"]["normal"] etc
+
+int main() {
+    double mass;
+    double x;
+    double y;
+    double z;
+    double Ixxtotal;
+    double Iyytotal;
+    double Izztotal;
+    double Ixytotal;
+    double Ixztotal;
+    double Iyztotal;
+
+    for(int i = 0; i < (craft["MassComponentNo"] - 1); i++)
+    {
+        mass = craft["MassComponents"][to_string(i)]["mass"];
+        x = craft["MassComponents"][to_string(i)]["position"][0u];
+        y = craft["MassComponents"][to_string(i)]["position"][1u];
+        z = craft["MassComponents"][to_string(i)]["position"][2u];
+        // mass calculations!
+        // Ixx = m(y^2 + z^2)
+        // Iyy = m(x^2 + z^2)
+        // Izz = m(x^2 + y^2)
+        // Ixy = -mxy
+        // Ixz = -mxz
+        // Iyz = -myz
+        // Values are sum of moments of inertia for the point masses
+
+        double Ixx = mass*(y^2 + z^2);
+        Ixxtotal += Ixx;
+        double Iyy = mass*(x^2 + z^2);
+        Iyytotal += Iyy;
+        double Izz = mass*(x^2 + y^2);
+        Izztotal += Izz;
+        double Ixy = -mass*x*y;
+        Ixytotal += Ixy;
+        double Ixz = -mass*x*z;
+        Ixztotal += Ixz;
+        double Iyz = -mass*y*z;
+        Iyztotal += Iyz;
+    }
+}
