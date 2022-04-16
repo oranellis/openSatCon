3e24a74d429b60ff118b2f347d574253703f0414
Aidan <2306781m@student.gla.ac.uk>
2022-04-16 14:15:34 +0100

    Implemented position dynamic model with gravitational acceleration

--- osc-daemon/bodydynamics.cpp
+++ osc-daemon/bodydynamics.cpp
@@ -17,14 +17,26 @@ namespace osc{
         dotstates.q3=0.5*curstates.q3;
     };
 
-    posstates positiondynamicmodel(posstates curstates){//work in progress
+    posstates positiondynamicmodel(posstates curstates, posstates noise){//work in progress
+    double r = sqrt(pow(curstates.i,2)+pow(curstates.j,2)+pow(curstates.k,2));
+    double C =  (-3*planet.J2*planet.sgp*pow(planet.sMa,2))/(2*pow(r,5))
+                    *(1-(5*pow(curstates.k,2)/pow(r,2))); //constant value
+        eci accgrav;
+        accgrav.i=C*curstates.i;
+        accgrav.j=C*curstates.j;
+        accgrav.k=C*curstates.k;
         posstates dotstates;
-        dotstates.x=0;
-        dotstates.y=0;
-        dotstates.z=0;
-        dotstates.vx=0;
-        dotstates.vy=0;
-        dotstates.vz=0;
+        dotstates.i=curstates.vi+noise.i; //ensure that these are ECI not ECEF
+        dotstates.j=curstates.vj+noise.j;
+        dotstates.k=curstates.vk+noise.k;
+        dotstates.vi=-planet.sgp*curstates.i/pow(r,3)+accgrav.i+noise.vi;
+        dotstates.vj=-planet.sgp*curstates.j/pow(r,3)+accgrav.j+noise.vj;
+        dotstates.vk=-planet.sgp*curstates.k/pow(r,3)+accgrav.k+noise.vk;
+    };
+
+    eci gravitationalacceleration(eci arg){
+        double r = sqrt(pow(arg.i,2)+pow(arg.j,2)+pow(arg.k,2));
+
     };
 
 }
