24f1a6b4c1426edffa4eef2686cf7606908901d1
Aidan <2306781m@student.gla.ac.uk>
2022-04-16 02:50:24 +0100

    Finished Axis Transformed, added more planet constants,

--- osc-daemon/bodydynamics.cpp
+++ osc-daemon/bodydynamics.cpp
@@ -7,8 +7,8 @@
 
 namespace osc{
 
-    states dynamicmodel(states curstates, states control, inertiamatrix inverseinertia){//someone make matrices inversible pls
-    states dotstates;
+    rotstates rotationdynamicmodel(rotstates curstates, rotstates control, inertiamatrix inverseinertia){//someone make matrices inversible pls
+    rotstates dotstates;
         dotstates.o1=inverseinertia.J11*control.o1+inverseinertia.J12*control.o2+inverseinertia.J13*control.o3;
         dotstates.o2=inverseinertia.J21*control.o1+inverseinertia.J22*control.o2+inverseinertia.J23*control.o3;
         dotstates.o3=inverseinertia.J31*control.o1+inverseinertia.J32*control.o2+inverseinertia.J33*control.o3;
@@ -16,4 +16,15 @@ namespace osc{
         dotstates.q2=0.5*curstates.q2;
         dotstates.q3=0.5*curstates.q3;
     };
+
+    posstates positiondynamicmodel(posstates curstates){//work in progress
+        posstates dotstates;
+        dotstates.x=0;
+        dotstates.y=0;
+        dotstates.z=0;
+        dotstates.vx=0;
+        dotstates.vy=0;
+        dotstates.vz=0;
+    };
+
 }
