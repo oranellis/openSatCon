278e65723d225bd1d4f55763b9c61cef8cf5044d
Shreyas Raje <2313623R@student.gla.ac.uk>
2022-04-17 11:37:21 +0100

    updated satellite build and parse json

--- osc-daemon/CraftBuild/parsejson.cpp
+++ osc-daemon/CraftBuild/parsejson.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include "../../includes/json.hpp"
 #include <list>
+#include <vector>
+#include <fstream>
+//#include "../components/component.hpp"
 
 using namespace std;
 using json = nlohmann::json;
@@ -8,28 +11,34 @@ using json = nlohmann::json;
 // change name to parse()
 int main() 
 {
-    std::cout << "start";
-    map<string, std::list<double> > Components;
+    std::cout << "start \n";
+
+    std::ifstream ifs("craft.json");
+    json j = json::parse(ifs);
+
+    std::vector<Test> Tests (j["MassComponentNo"]);
     
-    json j = json::parse("craft.json");
-    // std::ifstream ifs("craft.json");
-    // json j = json::parse(ifs);
-
-    // std::string str(R"({"json": "beta"})");
-    // json js = json::parse(str);
-
-    //parse mass components
-    for(int i = 0; i < 2; i++)
+    for(int i = 0; i < j["MassComponentNo"]; i++)
     {
-        //access data through j["name"]["name"]
-        Components.insert(std::make_pair(std::to_string(i), j["MassComponents"][std::to_string(i)]["componentType"]));
-        Components.insert(std::make_pair(std::to_string(i), j["MassComponents"][std::to_string(i)]["mass"]));
-        Components.insert(std::make_pair(std::to_string(i), j["MassComponents"][std::to_string(i)]["moi"]));
-        Components.insert(std::make_pair(std::to_string(i), j["MassComponents"][std::to_string(i)]["position"]));
-        Components.insert(std::make_pair(std::to_string(i), j["MassComponents"][std::to_string(i)]["powermodel"]));
+        std::cout<< j["MassComponents"][std::to_string(i)]["componentType"];
     }
 
-    std::cout << "Components";
+    //std::multimap<string, std::list<double> > Components;
+    
+    //std::vector<component> Components (j["MassComponentNo"]);
+
+    //parse mass components
+    // for(int i = 0; i < 2; i++)
+    // {
+    //     //access data through j["name"]["name"]
+    //     Components.insert(std::make_pair(std::to_string(i), j["MassComponents"][std::to_string(i)]["componentType"]));
+    //     Components.insert(std::make_pair(std::to_string(i), j["MassComponents"][std::to_string(i)]["mass"]));
+    //     Components.insert(std::make_pair(std::to_string(i), j["MassComponents"][std::to_string(i)]["moi"]));
+    //     Components.insert(std::make_pair(std::to_string(i), j["MassComponents"][std::to_string(i)]["position"]));
+    //     Components.insert(std::make_pair(std::to_string(i), j["MassComponents"][std::to_string(i)]["powermodel"]));
+    // }
+
+    //std::cout << Components;
     //parse thrust components
 }
 // take in json data 
