9663c5896f4eaefa8f8907bdf040eaabcc7a1675
Oran Ellis <oran.mcnair@gmail.com>
2022-04-18 16:46:30 +0100

    Fixed object/instance mixup

--- osc-daemon/datainjest/jsonparser.hpp
+++ osc-daemon/datainjest/jsonparser.hpp
@@ -36,46 +36,47 @@ craftconfig parseJson(std::string jsonPath) {
     //json j = json::parse(jsonPath);
     std::ifstream ifs(jsonPath);
     json j = json::parse(ifs);
+    craftconfig returnConfig;
 
     //parse and map mass components
     for(int i = 0; i < j["MassComponentNo"]; i++)
     {
-        craftconfig.components.insert (std::pair<std::string, component.mass>(j["MassComponents"][std::to_string(i)]["key"], j["MassComponents"][std::to_string(i)]["mass"]));
-        craftconfig.components.insert (std::pair<std::string, component.moi>(j["MassComponents"][std::to_string(i)]["key"], j["MassComponents"][std::to_string(i)]["moi"]));
-        craftconfig.components.insert (std::pair<std::string, component.pos>(j["MassComponents"][std::to_string(i)]["key"], j["MassComponents"][std::to_string(i)]["position"]));
-        craftconfig.components.insert (std::pair<std::string, component.power>(j["MassComponents"][std::to_string(i)]["key"], j["MassComponents"][std::to_string(i)]["powermodel"]));
+        returnConfig.components.insert (std::pair<std::string, component.mass>(j["MassComponents"][std::to_string(i)]["key"], j["MassComponents"][std::to_string(i)]["mass"]));
+        returnConfig.components.insert (std::pair<std::string, component.moi>(j["MassComponents"][std::to_string(i)]["key"], j["MassComponents"][std::to_string(i)]["moi"]));
+        returnConfig.components.insert (std::pair<std::string, component.pos>(j["MassComponents"][std::to_string(i)]["key"], j["MassComponents"][std::to_string(i)]["position"]));
+        returnConfig.components.insert (std::pair<std::string, component.power>(j["MassComponents"][std::to_string(i)]["key"], j["MassComponents"][std::to_string(i)]["powermodel"]));
 
-        std::cout << craftconfig.components["thruster"];
+        std::cout << returnConfig.components["thruster"];
     }
 
     // parse and map thruster components
     for(int i = 0; i < j["ThrustComponentNo"]; i++)
     {
-        craftconfig.thrusters.insert (std::pair<std::string, thruster.maxThrust>(j["ThrustComponents"][std::to_string(i)]["key"], j["MassComponents"][std::to_string(i)]["maxThrust"]));
-        craftconfig.thrusters.insert (std::pair<std::string, thruster.minThrustFraction>(j["ThrustComponents"][std::to_string(i)]["key"], j["MassComponents"][std::to_string(i)]["minThrustFraction"]));
-        craftconfig.thrusters.insert (std::pair<std::string, thruster.ThrustFraction>(j["ThrustComponents"][std::to_string(i)]["key"], j["MassComponents"][std::to_string(i)]["ThrustFraction"]));
-        craftconfig.thrusters.insert (std::pair<std::string, thruster.specificImpulse>(j["ThrustComponents"][std::to_string(i)]["key"], j["MassComponents"][std::to_string(i)]["specificImpulse"]));
-        craftconfig.thrusters.insert (std::pair<std::string, thruster.thrustAxis>(j["ThrustComponents"][std::to_string(i)]["key"], j["MassComponents"][std::to_string(i)]["thrustAxis"]));
+        returnConfig.thrusters.insert (std::pair<std::string, thruster.maxThrust>(j["ThrustComponents"][std::to_string(i)]["key"], j["MassComponents"][std::to_string(i)]["maxThrust"]));
+        returnConfig.thrusters.insert (std::pair<std::string, thruster.minThrustFraction>(j["ThrustComponents"][std::to_string(i)]["key"], j["MassComponents"][std::to_string(i)]["minThrustFraction"]));
+        returnConfig.thrusters.insert (std::pair<std::string, thruster.ThrustFraction>(j["ThrustComponents"][std::to_string(i)]["key"], j["MassComponents"][std::to_string(i)]["ThrustFraction"]));
+        returnConfig.thrusters.insert (std::pair<std::string, thruster.specificImpulse>(j["ThrustComponents"][std::to_string(i)]["key"], j["MassComponents"][std::to_string(i)]["specificImpulse"]));
+        returnConfig.thrusters.insert (std::pair<std::string, thruster.thrustAxis>(j["ThrustComponents"][std::to_string(i)]["key"], j["MassComponents"][std::to_string(i)]["thrustAxis"]));
 
-        std::cout << craftconfig.components["thruster"];
+        std::cout << returnConfig.components["thruster"];
     }
 
     // parse and map fuel tanks
     for(int i = 0; i < j["FuelTankNo"]; i++)
     {
-        craftconfig.fueltanks.insert (std::pair<std::string, fueltank.fuelMass>(j["FuelTanks"][std::to_string(i)]["key"], j["FuelTanks"][std::to_string(i)]["FuelMass"]));
-        craftconfig.fueltanks.insert (std::pair<std::string, fueltank.fuelMassPos>(j["FuelTanks"][std::to_string(i)]["key"], j["FuelTanks"][std::to_string(i)]["position"]));
+        returnConfig.fueltanks.insert (std::pair<std::string, fueltank.fuelMass>(j["FuelTanks"][std::to_string(i)]["key"], j["FuelTanks"][std::to_string(i)]["FuelMass"]));
+        returnConfig.fueltanks.insert (std::pair<std::string, fueltank.fuelMassPos>(j["FuelTanks"][std::to_string(i)]["key"], j["FuelTanks"][std::to_string(i)]["position"]));
 
-        std::cout << craftconfig.components["fuel_tank"];
+        std::cout << returnConfig.components["fuel_tank"];
     }
 
     // parse and map rotators
     for(int i = 0; i < j["ReactionWheelNo"]; i++)
     {
-        craftconfig.rotators.insert (std::pair<std::string, rotator.reactionWheel.Torque>(j["Rotators"][std::to_string(i)]["key"], j["Rotators"][std::to_string(i)]["Torque"]));
-        craftconfig.rotators.insert (std::pair<std::string, rotator.reactionWheel.RotationSpeed>(j["Rotators"][std::to_string(i)]["key"], j["Rotators"][std::to_string(i)]["RotationSpeed"]));
+        returnConfig.rotators.insert (std::pair<std::string, rotator.reactionWheel.Torque>(j["Rotators"][std::to_string(i)]["key"], j["Rotators"][std::to_string(i)]["Torque"]));
+        returnConfig.rotators.insert (std::pair<std::string, rotator.reactionWheel.RotationSpeed>(j["Rotators"][std::to_string(i)]["key"], j["Rotators"][std::to_string(i)]["RotationSpeed"]));
 
-        std::cout << craftconfig.components["rotator_1"];
+        std::cout << returnConfig.components["rotator_1"];
     }
 
 // access data via craft["ThrusterComponents"]["normal"] etc
