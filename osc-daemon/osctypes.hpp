58df95b378d141a7b0cf194c72bf9fe1b97db216
Oran Ellis <oran.mcnair@gmail.com>
2022-04-18 15:51:29 +0100

    Added vec3 cross product

--- osc-daemon/osctypes.hpp
+++ osc-daemon/osctypes.hpp
@@ -33,11 +33,18 @@ namespace osc {
         operator std::array<double, 3> () { return data; };
 
         // Member functions
-        vec3 dot(vec3 argPos) {
+        vec3 dot(vec3 arg) {
             /*
             Performs the vector dot product with the argument
             */
-            return position(data[0] * argPos[0], data[1] * argPos[1], data[2] * argPos[2]);
+            return vec3(data[0] * arg[0], data[1] * arg[1], data[2] * arg[2]);
+        }
+
+        vec3 cross(vec3 arg) {
+            /*
+            Performs the vector cross product with the argument
+            */
+            return vec3(data[1]*arg[2] - data[2]*arg[1], data[2]*arg[0] - data[0]*arg[2], data[0]*arg[1] - data[1]*arg[0]);
         }
     };
 
