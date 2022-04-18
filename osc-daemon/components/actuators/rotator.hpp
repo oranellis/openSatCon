59156b6e977f59ec391cc75e54a6119ebea30988
Shreyas Raje <2313623R@student.gla.ac.uk>
2022-04-18 23:01:45 +0100

    removed old code

--- osc-daemon/components/actuators/rotator.hpp
+++ osc-daemon/components/actuators/rotator.hpp
@@ -51,12 +51,6 @@ namespace osc {
       }
   };
 
-  class magneticTorquer {
-    private:
-      double MagneticFluxDensity;
-    public:
-      double GetMagneticFluxDensity(){
-        return MagneticFluxDensity;}
         //Torque  = DipoleMoment(Am^2) * MagneticFieldStrength(T)
         //        = DipoleMoment(Am^2) * (EarthMagneticMoment(Tm^3) / SatelliteRadius(m)^3 * lambda)
 
@@ -65,25 +59,7 @@ namespace osc {
         //lambda = sqrt(1 + (3 * sin(pi/2 - MagneticLatitude)^2))
 
         //MagneticLatitude = lookup table (International Geomagnetic Reference Field (IGRF))
-  };
 
-  class reactionWheel {
-    private:
-      double Torque, StoredMomentum, RotationSpeed;
-      std::array<double,3> normal;
-    public:
-      double GetTorque(){
-        return Torque;}
-      double GetStoredMomentum(){
-        return StoredMomentum;}
-      double GetRotationSpeed(){
-        return RotationSpeed;}
-        //Torque  = RotationSpeed * Something ()
-  };
-
-  class rotator {
-
-  };
 }
 
 #endif // ROTATOR_H
