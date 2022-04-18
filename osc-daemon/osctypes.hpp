17c2a72c9fdad7b6e5e9878f1aa9e9d5d12cf46c
Aidan <2306781m@student.gla.ac.uk>
2022-04-18 14:09:58 +0100

    added pow2 function for comp speed increase

--- osc-daemon/osctypes.hpp
+++ osc-daemon/osctypes.hpp
@@ -183,6 +183,8 @@ namespace osc {
         std::vector<double> pstates; 
     };
 
+double pow2(double x){return x*x;}// use this to replace pow(x,n) if n=2 and speed must =fast.lots
+
 }
 
 #endif
