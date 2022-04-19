0958d141f6d9f47635bd24327d125647dd5eb500
Shreyas Raje <2313623R@student.gla.ac.uk>
2022-04-19 19:58:11 +0100

    Doxygen documentation

--- osc-daemon/components/actuators/thruster.hpp
+++ osc-daemon/components/actuators/thruster.hpp
@@ -7,47 +7,58 @@
 
 namespace osc {
 
+  /** \class thruster 
+  Thruster class for thrust producing components
+  */
   class thruster {
 
     private:
-
+    
     // Class variable initialisers
-    double maxThrust; // (N)
+    /// @param maxThrust maximum thrust in Newtons (N)
+    double maxThrust; 
+    /// @param minThrustFraction minimum thrust fraction, representing minimum configurable thrust, between 0 and 1
     double minThrustFraction; // Value from 0 to 1 representing the minimum configurable thrust fraction
+    /// @param thrustFraction normal thrust fraction, between 0 and 1
     double thrustFraction;
+    /// @param specificImpulse specific impulse of thruster (s)
     double specificImpulse; // (s)
+    /// @param attitudeControl boolean to define whether the thruster is used for attitude control
     bool attitudeControl;
+    /// @param thrustAxis Vector defining the axis of the thrust
     vec3 thrustAxis;
+    /// @param thrustCentre position of the center of thrust
     position thrustCentre;
 
     public:
-    //Constructor
+    /// Constructor
     thruster(double initMaxThrust, double initMinFrac, double initThrustFrac, double initSpecificImpulse, 
     bool initAttitudeControl, vec3 initThrustAxis, position initThrustCentre):
       maxThrust(initMaxThrust), minThrustFraction(initMinFrac), thrustFraction(initThrustFrac),
       specificImpulse(initSpecificImpulse), attitudeControl(initAttitudeControl), thrustAxis(initThrustAxis),
       thrustCentre(initThrustCentre) { }
 
+    /// returns \p maxThrust
     double getMaxThrust() {
       return maxThrust;
     }
-
+    /// returns \p specificImpulse
     double getSpecificImpulse() {
       return specificImpulse;
     }
-
+    /// returns \p attitudeControl
     bool getAttitudeControl() {
       return attitudeControl;
     }
-
+    /// returns \p thrustAxis
     vec3 getThrustAxis() {
       return thrustAxis;
     }
-
+    /// returns \p thrustCentre
     position getThrustCentre() {
       return thrustCentre;
     }
-
+    /// sets the thrust fraction to a defined value, returns true if the setting is possible
     bool setThrustFraction(double argThrustFraction) {
       if (argThrustFraction<=1 && argThrustFraction>=minThrustFraction) {
         thrustFraction = argThrustFraction;
--- osc-daemon/components/component.hpp
+++ osc-daemon/components/component.hpp
@@ -8,42 +8,56 @@
 
 namespace osc {
 
+  /** \class component
+  Component class of a satellite object, used for the general definition
+  of components on a satellite
+  */
   class component{
 
     private:
+    /// @param mass Mass of the object
     double mass;
+    /// @param moi Moment of inertia of the object
     momentofinertia moi;
+    /// @param pos Position of the object
     position pos;
+    /// @param rot Quaternion rotation of the object
     quaternion rot;
+    /// @param power Powermodel used by object
     powermodel power;
 
     public:
     
-    // Constructors
+    /// Constructors
     component(double initMass, momentofinertia initMoi, position initPos, quaternion initRot, powermodel initPower):
       mass(initMass), moi(initMoi), pos(initPos), rot(initRot), power(initPower) { }
 
     // Accessers
+    /// returns \p mass
     double getMass() {
       return mass;
     }
-
+    /// returns \p moi
     momentofinertia getMoi() {
       return moi;
     }
     
+    /// returns \p pos
     position getPos() {
       return pos;
     }
 
+    /// returns \p power
     powermodel getPower() {
       return power;
     }
 
+    /// sets the mass of an object
     void setMass(double argMass) {
       mass = argMass;
     }
-
+    
+    /// sets the position of an object
     void setPos(position argPos) {
       pos = argPos;
     }
