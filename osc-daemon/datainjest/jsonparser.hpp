73046f09aeb7aa031d4cf6d65eb21f1ecfdec038
Shreyas Raje <2313623R@student.gla.ac.uk>
2022-04-19 12:00:01 +0100

    parses and maps json to relevant objects

--- osc-daemon/datainjest/jsonparser.hpp
+++ osc-daemon/datainjest/jsonparser.hpp
@@ -37,67 +37,112 @@ craftconfig parseJson(std::string jsonPath) {
     //json j = json::parse(jsonPath);
     std::ifstream ifs(jsonPath);
     json j = json::parse(ifs);
+
     craftconfig returnConfig;
 
     //parse and map mass components
     for(int i = 0; i < j["MassComponentNo"]; i++)
     {
         double mass = j["MassComponents"][std::to_string(i)]["mass"];
+        
         momentofinertia moi;
+        position pos;
+        
         moi.Ixx = j["MassComponents"][std::to_string(i)]["moi"][0u];
         moi.Iyy = j["MassComponents"][std::to_string(i)]["moi"][1u];
         moi.Izz = j["MassComponents"][std::to_string(i)]["moi"][2u];
-        position pos;
+        
         pos.x = j["MassComponents"][std::to_string(i)]["position"][0u];
         pos.y = j["MassComponents"][std::to_string(i)]["position"][1u];
         pos.z = j["MassComponents"][std::to_string(i)]["position"][2u];
+
         powermodel power;
-        std::cout << j["MassComponents"][std::to_string(i)]["powermodel"];
+        power.off = j["MassComponents"][std::to_string(i)]["powermodel"][0u];
+        power.idle = j["MassComponents"][std::to_string(i)]["powermodel"][1u];
+        power.use = j["MassComponents"][std::to_string(i)]["powermodel"][2u];
+        power.max = j["MassComponents"][std::to_string(i)]["powermodel"][3u];
+
         quaternion rot;
         component massComponent(mass, moi, pos, rot, power);
-
-        returnConfig.components.insert(std::pair<std::string, component>(j["MassComponents"][std::to_string(i)]["key"], massComponent));
+        std::string key = j["MassComponents"][std::to_string(i)]["key"];
+        returnConfig.components.insert(std::pair<std::string, component>(key, massComponent));
     }
 
     // parse and map thruster components
-    // for(int i = 0; i < j["ThrustComponentNo"]; i++)
-    // {
-    //     thruster thrustComponent;
-    //     thrustComponent.maxThrust = j["MassComponents"][std::to_string(i)]["maxThrust"];
-    //     thrustComponent.minThrustFraction = j["MassComponents"][std::to_string(i)]["minThrustFraction"];
-    //     thrustComponent.thrustFraction = j["MassComponents"][std::to_string(i)]["ThrustFraction"];
-    //     thrustComponent.specificImpulse = j["MassComponents"][std::to_string(i)]["specificImpulse"];
-    //     thrustComponent.thrustAxis = j["MassComponents"][std::to_string(i)]["thrustAxis"];
-
-    //     returnConfig.thrusters.insert (std::pair<std::string, thruster>(j["ThrustComponents"][std::to_string(i)]["key"], thrustComponent));
+    for(int i = 0; i < j["ThrusterComponentNo"]; i++)
+    {
+        double maxT = j["ThrusterComponents"][std::to_string(i)]["maxThrust"];
+        double mTFrac = j["ThrusterComponents"][std::to_string(i)]["minThrustFraction"];
+        double tFrac = j["ThrusterComponents"][std::to_string(i)]["thrustFraction"];
+        double spImpulse = j["ThrusterComponents"][std::to_string(i)]["specificImpulse"];
         
-    // }
+        bool attitudeControl;
+        if (j["ThrusterComponents"][std::to_string(i)]["attitudeControl"] == 0) {
+            attitudeControl = false;
+        } else {
+            attitudeControl = true;
+        }
+        
+        std::array<double, 3> tAxis;
+        tAxis[0] = j["ThrusterComponents"][std::to_string(i)]["thrustAxis"][0u];
+        tAxis[1] = j["ThrusterComponents"][std::to_string(i)]["thrustAxis"][1u];
+        tAxis[2] = j["ThrusterComponents"][std::to_string(i)]["thrustAxis"][2u];
+     
+        position tCenter;
+        tCenter.x = j["ThrusterComponents"][std::to_string(i)]["thrustCenter"][0u];
+        tCenter.y = j["ThrusterComponents"][std::to_string(i)]["thrustCenter"][1u];
+        tCenter.z = j["ThrusterComponents"][std::to_string(i)]["thrustCenter"][2u];
 
-    // // parse and map fuel tanks
-    // for(int i = 0; i < j["FuelTankNo"]; i++)
-    // {
-    //     fueltank FuelTank;
-    //     FuelTank.fuelMass = j["FuelTanks"][std::to_string(i)]["FuelMass"];
-    //     FuelTank.fuelMassPos = j["FuelTanks"][std::to_string(i)]["position"];
+        thruster ThrusterComponent(maxT, mTFrac, tFrac, spImpulse, attitudeControl, tAxis, tCenter);
+        
+        std::string key = j["ThrusterComponents"][std::to_string(i)]["key"];
+        returnConfig.thrusters.insert (std::pair<std::string, thruster>(key, ThrusterComponent));
+        
+    }
 
-    //     returnConfig.fueltanks.insert (std::pair<std::string, fueltank>(j["FuelTanks"][std::to_string(i)]["key"], FuelTank));
+    // parse and map fuel tanks
+    for(int i = 0; i < j["FuelTankNo"]; i++)
+    {
+        double fuelmass = j["FuelTanks"][std::to_string(i)]["FuelMass"];
+        double fuelcap = j["FuelTanks"][std::to_string(i)]["FuelCapacity"];
+        std::string fueltype = j["FuelTanks"][std::to_string(i)]["FuelType"];
+        position pos;
+        pos.x = j["FuelTanks"][std::to_string(i)]["position"][0u];
+        pos.y = j["FuelTanks"][std::to_string(i)]["position"][1u];
+        pos.z = j["FuelTanks"][std::to_string(i)]["position"][2u];
+        fueltank FuelTank(fueltype, fuelmass, fuelcap, pos);
 
-    //     // returnConfig.fueltanks.insert (std::pair<std::string, double>(j["FuelTanks"][std::to_string(i)]["key"], j["FuelTanks"][std::to_string(i)]["FuelMass"]));
-    //     // returnConfig.fueltanks.insert (std::pair<std::string, position>(j["FuelTanks"][std::to_string(i)]["key"], j["FuelTanks"][std::to_string(i)]["position"]));
-    // }
+        std::string key = j["FuelTanks"][std::to_string(i)]["key"];
+
+        returnConfig.fueltanks.insert (std::pair<std::string, fueltank>(key, FuelTank));
+    }
 
     // // parse and map rotators
-    // for(int i = 0; i < j["ReactionWheelNo"]; i++)
-    // {
-    //     returnConfig.rotators.insert (std::pair<std::string, double>(j["Rotators"][std::to_string(i)]["key"], j["Rotators"][std::to_string(i)]["Torque"]));
-    //     returnConfig.rotators.insert (std::pair<std::string, double>(j["Rotators"][std::to_string(i)]["key"], j["Rotators"][std::to_string(i)]["RotationSpeed"]));
-    // }
+    for(int i = 0; i < j["RotatorNo"]; i++)
+    {
+        double mDipole = j["Rotators"][std::to_string(i)]["maxDipoleMoment"];
+        double torque = j["Rotators"][std::to_string(i)]["Torque"];
+        double storedMomentum = j["Rotators"][std::to_string(i)]["storedMomentum"];
+        double rotSpeed = j["Rotators"][std::to_string(i)]["RotationSpeed"];
+        momentofinertia moi;
+        moi.Ixx = j["Rotators"][std::to_string(i)]["moi"][0u];
+        moi.Iyy = j["Rotators"][std::to_string(i)]["moi"][1u];
+        moi.Izz = j["Rotators"][std::to_string(i)]["moi"][2u];
+        std::array<double, 3> direction;
+        direction[0] = j["Rotators"][std::to_string(i)]["NormalVector"][0u];
+        direction[1] = j["Rotators"][std::to_string(i)]["NormalVector"][1u];
+        direction[2] = j["Rotators"][std::to_string(i)]["NormalVector"][2u];
+
+        rotator RotatorComponent(mDipole, torque, storedMomentum, rotSpeed, moi, direction);
+
+        std::string key = j["Rotators"][std::to_string(i)]["key"];
+
+        returnConfig.rotators.insert (std::pair<std::string, rotator>(key, RotatorComponent));
+    }
 
     return returnConfig;
-// access data via craft["ThrusterComponents"]["normal"] etc
-};
-}
 
+};
 }
 
 #endif // JSONPARSER_H
